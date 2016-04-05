079d363fa862da5c0d4e0d82c3b71fc089f14026
Laughing_Lz <645596034@qq.com>
2016-04-05 15:38:23 +0800

    添加Hello.java

---
+++ src/ren/laughing/datastructure/util/Hello.java
@@ -0,0 +1,12 @@
+package ren.laughing.datastructure.util;
+/**
+ * 欢迎访问我的项目
+ * 此为学习数据结构代码记录
+ * @author Laughing
+ * @time 2016年4月5日
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("愿诸君共勉");
+	}
+}
