2aff5ff15c8630af01c0e334e6d0508c40607291
Laughing_Lz <645596034@qq.com>
2016-04-22 20:39:59 +0800

    排序：update交换排序
    冒泡排序
    快速排序

--- src/ren/laughing/datastructure/algorithm/Sorter.java
+++ src/ren/laughing/datastructure/algorithm/Sorter.java
@@ -63,27 +63,104 @@ public class Sorter {
 	 *            步长序列
 	 */
 	public static void shellSort(int[] arr, int low, int high, int[] delta) {
-		for (int i = 0; i < delta.length; i++) {//第一步：遍历步长序列
+		for (int i = 0; i < delta.length; i++) {// 第一步：遍历步长序列
+			for (int m = low; m < low + delta[i]; m++) {// 第二步：循环起始点，保证每个被拆分的子序列都被直接排序
+				for (int j = m + delta[i]; j <= high; j += delta[i]) {// 对每个子序列直接排序
 					int temp = arr[j];
 					int k = j;
 					for (; k > m && temp < arr[(k - delta[i])]; k -= delta[i]) {
-						arr[k] = arr[(k - delta[i])];//后移
+						arr[k] = arr[(k - delta[i])];// 后移
 					}
-					arr[k] = temp;//插入
+					arr[k] = temp;// 插入
 				}
 			}
 		}
 		printResult("希尔排序：", arr);
 	}
 
+	/**
+	 * 冒泡排序 时间复杂度为O(n^2)
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	public static void bubbleSort(int[] arr, int low, int high) {
+		int len = high - low + 1;
+		for (int i = 1; i < len; i++) {
+			for (int j = low; j <= high - i; j++) {// 这里j<= high-i ★
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];// 交换
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		printResult("冒泡排序：", arr);
+	}
+
+	/**
+	 * 快速排序 需要递归
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	public static void quickSort(int[] arr, int low, int high) {
+		if (low < high) {
+			int pa = partition(arr, low, high);// 分治
+			quickSort(arr, low, pa - 1);
+			quickSort(arr, pa + 1, high);
+		}
+	}
+
+	/**
+	 * 将序列划分为两个子序列并返回枢轴元素的位置
+	 * 
+	 * @param arr
+	 * @param low
+	 *            划分区间
+	 * @param high
+	 * @return
+	 */
+	private static int partition(int[] arr, int low, int high) {
+		int pivot = arr[low];// 首先定义枢轴为low所指元素
+		while (low < high) {// 交替扫描
+			while (arr[high] > pivot && low < high) {
+				high--;
+			}
+			arr[low] = arr[high];// 将比 pivot 小的元素移向低端
+			while (arr[low] < pivot && low < high) {
+				low++;
+			}
+			arr[high] = arr[low];// 将比 pivot 大的元素移向高端
+		}
+		arr[low] = pivot;// 设置枢轴
+		return low;
+	}
+
+	/**
+	 * 程序入口
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int[] arr = new int[] { 2, 5, 7, 3, 4, 8, 1, 9, 6, 0 };
 		int[] delta = new int[] { 5, 3, 1 };
 		// insertSort(arr, 0, 9);
 		// binInsertSort(arr, 0, 9);
-		shellSort(arr, 0, 9, delta);
+		// shellSort(arr, 0, 9, delta);
+		// bubbleSort(arr, 0, 9);
+		quickSort(arr, 0, 9);
+		printResult("快速排序：", arr);
 	}
 
+	/**
+	 * 打印
+	 * 
+	 * @param str
+	 * @param arr
+	 */
 	public static void printResult(String str, int[] arr) {
 		System.out.print(str);
 		for (int i = 0; i < arr.length; i++) {
