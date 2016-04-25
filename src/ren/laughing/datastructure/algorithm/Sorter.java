9308fc3305a849fee1cf6f609882e3d198c7f969
Laughing_Lz <645596034@qq.com>
2016-04-25 18:45:37 +0800

    排序：update选择类排序&归并排序
    选择类排序：简单选择排序、堆排序
    树型选择排序看不懂代码T_T

--- src/ren/laughing/datastructure/algorithm/Sorter.java
+++ src/ren/laughing/datastructure/algorithm/Sorter.java
@@ -1,5 +1,13 @@
 package ren.laughing.datastructure.algorithm;
 
+import ren.laughing.datastructure.baseImpl.BinTreeNode;
+
+/**
+ * 排序
+ * 
+ * @author Laughing_Lz
+ * @time 2016年4月22日
+ */
 public class Sorter {
 	/**
 	 * 直接插入排序(从小到大) 时间复杂度为O(n^2)
@@ -140,6 +148,120 @@ public class Sorter {
 	}
 
 	/**
+	 * 简单选择排序 时间复杂度为O(n^2)
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	public static void selectSort(int[] arr, int low, int high) {
+		for (int i = low; i < high - 1; i++) {
+			int min = i;// 记录位置,不是元素是位置！★
+			for (int j = i; j <= high; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;// 更新最小元素位置
+				}
+			}
+			if (min != i) {// 交换
+				int temp = arr[min];
+				arr[min] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		printResult("简单选择排序：", arr);
+	}
+
+	/**
+	 * 堆排序，时间复杂度为O(nlogn) 堆排序中，arr[]数组的首位arr[0]弃用。所以这里arr[1]为根结点
+	 * 
+	 * @param arr
+	 */
+	public static void heapSelectSort(int[] arr) {
+		int n = arr.length - 1;
+		for (int i = n / 2; i >= 1; i--) {// 遍历叶子结点的上层第一个结点及之前所有结点,i>=1是因为数组中首位元素弃用
+			heapAdjust(arr, i, n);// 初始化建堆,i=n/2位置
+		}
+		for (int i = n; i > 1; i--) {//
+			int temp = arr[1];// 堆顶元素（根节点）为最大元素，将它和堆底元素交换
+			arr[1] = arr[i];
+			arr[i] = temp;
+			heapAdjust(arr, 1, i - 1);// 从根结点更新堆
+		}
+		printResult("堆排序：", arr);
+	}
+
+	/**
+	 * 创建堆
+	 * 
+	 * @param r
+	 *            数组元素根据二叉树层次遍历顺序存储
+	 * @param low
+	 *            遍历是否比左右孩子大的起始结点
+	 * @param high
+	 *            数组长度
+	 */
+	private static void heapAdjust(int[] r, int low, int high) {
+		int temp = r[low];// r[low]为要与左右孩子比较的父结点，以保证其比左右孩子都大
+		for (int j = 2 * low; j <= high; j = j * 2) {// 需要循环保证该起始结点low以下的树都符合堆定义★
+			if (j < high && r[j] < r[j + 1]) {// r[j]是r[low]的左孩子，r[j+1]是右孩子
+				j++;
+			}
+			if (temp > r[j]) {// 父结点与较大的孩子比较大小，若父结点小，交换
+				break;
+			}
+			r[low] = r[j];// 父结点存放的是三者中最大的元素
+			low = j;
+		}
+		r[low] = temp;// 将原父结点的元素存入孩子结点
+	}
+
+	/**
+	 * 归并排序
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	public static void mergeSort(int[] arr, int low, int high) {
+		if (low < high) {// 分治
+			mergeSort(arr, low, (low + high) / 2);
+			mergeSort(arr, (low + high) / 2 + 1, high);
+			merge(arr, low, (low + high) / 2, high);
+		}
+	}
+
+	/**
+	 * 将两个有序区间[low,mid]和[mid+1,high]合并成一个有序区间
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param mid
+	 * @param high
+	 */
+	private static void merge(int[] arr, int low, int mid, int high) {
+		int[] temp = new int[high - low + 1];
+		int a = low;
+		int b = mid + 1;
+		int t = 0;
+		while (a <= mid && b <= high) {// 注意这个循环★
+			if (arr[a] < arr[b]) {
+				temp[t++] = arr[a++];
+			} else {
+				temp[t++] = arr[b++];
+			}
+		}
+		while (a <= mid) {
+			temp[t++] = arr[a++];
+		}
+		while (b <= high) {
+			temp[t++] = arr[b++];
+		}
+		for (int i = 0; i < temp.length; i++) {
+			arr[low + i] = temp[i];
+		}
+	}
+
+	/**
 	 * 程序入口
 	 * 
 	 * @param args
@@ -151,8 +273,12 @@ public class Sorter {
 		// binInsertSort(arr, 0, 9);
 		// shellSort(arr, 0, 9, delta);
 		// bubbleSort(arr, 0, 9);
-		quickSort(arr, 0, 9);
-		printResult("快速排序：", arr);
+		// quickSort(arr, 0, 9);
+		// printResult("快速排序：", arr);
+		// selectSort(arr, 0, 9);
+		// heapSelectSort(arr);
+		mergeSort(arr, 0, 9);
+		printResult("归并排序：", arr);
 	}
 
 	/**
